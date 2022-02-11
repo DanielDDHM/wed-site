3da5c9acda06cc659782a9b95732351f8b517651
DanielDDHM <daniel.ddhm@gmail.com>
2022-02-11 11:34:40 -0300

    feat: languages

--- src/components/welcome-area/index.js
+++ src/components/welcome-area/index.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
 import './style.css'
 
 const Welcome = (props) => {
@@ -22,6 +22,7 @@ const Welcome = (props) => {
                     <div className="welcome-content">
                         <h2>Welcome to our big day</h2>
                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or less normal distribution of letters</p>
+                        <div className="btn"><AnchorLink href='#rules'>Rules</AnchorLink></div>
                         <div className="btn"><AnchorLink href='#rsvp'>Rsvp</AnchorLink></div>
                         <Button className="btn" onClick={toggle}>Location</Button>
                         <Modal isOpen={modal} toggle={toggle} className={className}>
